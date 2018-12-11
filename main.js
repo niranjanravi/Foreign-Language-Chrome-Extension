7c2f8066c129e614fa900b84ea6532002a43a858
Niranjan Ravi <nr339@cornell.edu>
2018-12-10 20:02:33 -0500

    Remove unncessary comments

--- main.js
+++ main.js
@@ -59,9 +59,7 @@ function generateId() {
 /* Return closest parent element of given class. */
 function getFbId(elt) {
     var id = elt.find("._5pcq")[0]["href"];
-    // var id = elt.find('._43f9._63qh a ~ span')[0]["id"]
     id = id.match(/\d{15}/g).slice(-1)[0];
-    // id = id.match(/\d{16}/g)[0]
     return id;
 }
 
@@ -91,23 +89,6 @@ $(document).ready(function() {
       nativeLang = obj.nativelangs;
     }
   });
-
-  // $("._khz").click(function (event) {
-  //   console.log('LIKE');
-  //   var fbIdClick = getFbId($(event.target).closest("._5pcr"));
-  //   if (analyzedFbId.has(fbIdClick)) {
-  //     var logBlob = {
-  //       time: Date.now() - 18000000,
-  //       postID: fbIdClick.substring(5),
-  //       like: 1
-  //     };
-  //     var payLoad = {
-  //       name: myName,
-  //       logBlob: logBlob
-  //     };
-  //     chrome.runtime.sendMessage(payLoad, function(response){});
-  //   }
-  // });
 
   $('._3t54').unbind().bind("DOMSubtreeModified", function() {
     var fbIdClick = getFbId($(event.target).closest("._5pcr"));
@@ -307,10 +288,6 @@ $(document).ready(function() {
           }
 
 
-          // nlp.parse(translatedText).then(output => {
-          //   console.log(output[0].noun_phrases);
-          // })
-
           if (textrazorSupported.indexOf(language) >= 0) {
             // run entity analysis in original language, if supported
             langCode = textrazorCodes[textrazorSupported.indexOf(language)];
@@ -319,10 +296,6 @@ $(document).ready(function() {
             // otherwise run analysis on translation
             runEntityAnalysis(translatedText, "eng", linkSection);
           }
-          // run emotion analysis only on user content
-        //   if (!linkSection) {
-        //     runEmotionAnalysis(translatedText, content);
-        //   }
       }
       else{ //if language is not english
 
@@ -378,10 +351,6 @@ $(document).ready(function() {
               translatedTextFull += " " + translatedText;
             }
         }, 1000);
-
-        //   $.get(translateUrl + content, function(data) {
-        //   translatedText = data.data.translations[0].translatedText;
-        //   // console.log("After translation: " + translatedText);
       }
     }
 
@@ -415,14 +384,6 @@ $(document).ready(function() {
     }
 
     function detectLanguage(content) {
-      //console.log("FB ID:", fbId);
-      // var translateButton = getTranslationButton();
-      // translateButton.click(function(){
-      //   addHoverBox();
-      //   createButton();
-      //   addTranslationBox();
-      //   translateText(main, language1, false);
-      // });
       var isNative = false; //if language is native
       if (!(fbId in fbIds)) {
         console.log("FB ID: " + fbId);
@@ -517,12 +478,9 @@ $(document).ready(function() {
         $("#white-box2").css('padding', '10px');
         $("#white-box2").css('width', 'auto');
         $("#white-box2").css('text-align', 'center');
-        // if ($("#white-box2").text() == '') {
-         
-        // }
+
         $("#white-box2").css('display', 'inline-block');
         $("#white-box2").css('width', 'calc(100% - 20px)');
-        //$("#white-box2").hide();
         var cleanText = "";
         if (linkSection) {
           cleanText = translatedTextOrig + "<hr><p><b>Link:</b></p>" + translatedText;
@@ -548,7 +506,6 @@ $(document).ready(function() {
 
       $("#white-box").empty();
       $("#white-box").append(content);
-      //$("#white-box2").slideToggle();
 
 
       $('.entity').unbind().click(function(){
@@ -608,269 +565,6 @@ $(document).ready(function() {
         $("#wiki-text").css('overflow', 'hidden');
         $("#wiki-text").css('padding-top', 'hidden');
       });
-
-      // $("#entityToggle").click(function(){
-        
-      // });
-
-      var hasEntity = false;
-      // $.ajax({
-      //   url: "https://api.textrazor.com",
-      //   type: "POST",
-      //   headers: {
-      //     "X-TextRazor-Key": textRazorKey3
-      //   },
-      //   data: {
-      //     text: content,
-      //     extractors: "entities",
-      //     languageOverride: langCode,
-      //   },
-      //   success: function(res) {
-      //     // only add label and box for entity analysis once
-          
-
-      //     // if entities detected, replace matched text in translation with entity object
-      //     var entitiesArray = []; // separate for user content and link content
-      //     if (res.response.hasOwnProperty("entities")) {
-      //       // build array of entities
-      //       for (var i = 0; i < res.response.entities.length; i++) {
-      //         var type = res.response.entities[i].type;
-      //         // don't include entities of the following types
-      //         if ((type != undefined) && res.response.entities[i].confidenceScore > .5 ||
-      //           res.response.entities[i].wikiLink.length > 0) {
-      //           if ((type != undefined) && !(type[0] == "Number" || type[0] == "Money" || type[0] == "Email" ||
-      //               type[0] == "URL" || type[0] == "Duration" || type[0] == "Date" ||
-      //               type[0] == "Time" || type[0] == "Ordinal" || type[0] == "Percentage" ||
-      //               type[0] == "Temperature" || type[0] == "Distance" || type[0] == "TimePeriod")) {
-      //             entitiesArray.push(res.response.entities[i]);
-      //           } else if (type == undefined) {
-      //             entitiesArray.push(res.response.entities[i]);
-      //           }
-      //         }
-      //       }
-            
-      //       // build arrays of matched words and entities
-      //       var push;
-      //       var translation; // english version of matched text for single entity
-      //       for (var i = 0; i < entitiesArray.length; i++) {
-      //         push = true;
-      //         var matchedText = String(entitiesArray[i]["matchedText"]).replace(/#/g, "").replace(/&/g, "");
-      //         if (matchedText.indexOf("<") >= 0) {
-      //           matchedText = matchedText.substr(0, matchedText.indexOf("<"));
-      //         }
-      //         $.get(translateUrl + matchedText, function(data) {
-      //           // if foreign text is not directly in translation, use translation of matched text
-      //           if (textrazorSupported.indexOf(linkSection ? language2 : language1) >= 0 &&
-      //             translatedText.toLowerCase().indexOf(matchedText.toLowerCase()) < 0) {
-      //             translation = data.data.translations[0].translatedText;
-      //           } else {
-      //             translation = matchedText;
-      //           }
-      //           // if one is contained within the other, don't include shorter one
-      //           // && translatedText.toLowerCase().indexOf(translation.toLowerCase()) >= 0
-      //           if (matchedWords.length > 0) {
-      //             for (var j = 0; j < matchedWordsLower.length; j++) {
-      //               if (matchedWordsLower[j].indexOf(translation.toLowerCase()) >= 0 || translation.toLowerCase().indexOf(matchedWordsLower[j]) >= 0) {
-      //                 if (matchedWordsLower[j].length < translation.length) {
-      //                   matchedWords.splice(j, 1);
-      //                   matchedWordsLower.splice(j, 1);
-      //                   selectedEntities.splice(j, 1);
-      //                 } else {
-      //                   push = false;
-      //                 }
-      //               }
-      //             }
-      //             // prevent duplicates
-      //             if (matchedWordsLower.indexOf(translation.toLowerCase()) < 0 && push) {
-      //               matchedWords.push(translation);
-      //               matchedWordsLower.push(translation.toLowerCase());
-      //               selectedEntities.push(entitiesArray[i]);
-      //             }
-      //           } else {
-      //             matchedWords.push(translation);
-      //             matchedWordsLower.push(translation.toLowerCase());
-      //             selectedEntities.push(entitiesArray[i]);
-      //           }
-      //         });
-      //       }
-
-      //       // highlight matched text in translation,
-      //       // only if link content entities have already been included or no link content 
-      //       if (linkSection || linkText.length == 0) {
-      //         // build array of entity information for log
-      //         allEntities = [];
-      //         var json;
-      //         var entityType;
-      //         selectedEntityTypes = [];
-
-      //         for (var i = 0; i < selectedEntities.length; i++) {
-      //           entityType = String(selectedEntities[i]["type"]).replace(/([A-Z])/g, ' $1').trim();
-      //           selectedEntityTypes.push(entityType);
-      //           json = '{ "matchedText": "' + selectedEntities[i]["matchedText"] + '", ';
-      //           json += '"type": "' + entityType + '", ';
-      //           json += '"wikiLink": "' + selectedEntities[i]["wikiLink"] + '" }';
-      //           allEntities.push(JSON.parse(json));
-      //         }
-
-      //         // highlight matched text in translation
-      //         if (linkSection && $("#white-box").text().length > 0) {
-      //           var newText = translatedTextOrig + "<hr><p><b>Link:</b></p>" + translatedText;
-      //         } else if (linkSection) {
-      //           var newText = translatedTextOrig + "<hr><p><b>Link:</b></p>" + translatedText;
-      //         } else {
-      //           var newText = translatedTextOrig;
-      //         }
-      //         // replace matched words with unique entity id
-      //         var entityIds = [];
-      //         var orderedMatches = [];
-      //         for (var i = 0; i < matchedWords.length; i++) {
-      //           var regex = new RegExp(matchedWords[i].replace(".", "/."), 'ig'); // case insensitive search
-      //           if (newText.match(regex) != null) {
-      //             var index = 0;
-      //             var count = newText.match(regex).length;
-      //             for (var j = 0; j < count; j++) {
-      //               // create unique id for each highlighted text
-      //               var entityId = matchedWords[i].replace(/&#,'"[<>\\+$~%*{}]/g, "").replace(/ /g, "").toLowerCase() + String(i * 10 + j);
-      //               entityIds.push(entityId);
-      //               orderedMatches.push(matchedWords[i]);
-      //               if (uniqueEntities.indexOf(matchedWords[i]) < 0) {
-      //                 uniqueEntities.push(matchedWords[i]);
-      //               }
-      //               // index is index of next occurrence
-      //               index = newText.toLowerCase().indexOf(matchedWords[i].toLowerCase(), index);
-      //               newText = newText.substring(0, index) + entityId +
-      //                 newText.substring(index + matchedWords[i].length);
-      //               index = index + entityId.length; // end of last replacement
-      //             }
-      //           }
-      //         }
-      //         // replace entity ids with highlighted text
-      //         for (var i = 0; i < entityIds.length; i++) {
-      //           var highlighted = "<a href='#'><mark class='entity' id=" + entityIds[i] +
-      //             " style='background-color: #ffb3b3; opacity: .75;'>" + orderedMatches[i] + "</mark></a>";
-      //           newText = newText.replace(entityIds[i], highlighted);
-      //         }
-
-      //         if (entityIds.length > 0) {
-      //           hasEntity = true;
-      //         }
-      //         if (linkSection) {
-      //           cleanText = translatedTextOrig + "<hr><p><b>Link:</b></p>" + translatedText;
-      //         } else {
-      //           cleanText = translatedText;
-      //         }
-      //         $("#white-box").empty();
-      //         $("#white-box").append(cleanText);
-
-      //         if (linkText.length === 0 &&
-      //           (document.getElementsByClassName("entity").length === 0 || selectedEntities.length === 0)) {}
-      //         if (linkText !== "" && linkSection &&
-      //           (document.getElementsByClassName("entity").length === 0 || selectedEntities.length === 0)) {}
-
-      //         function entityClick() { // display single entity
-      //           $(".entity").unbind().click(function() {
-      //             var clickedEntity = $(this).text();
-      //             // change highlight color of other entities back to red
-      //             var entities = document.getElementsByClassName("entity");
-      //             for (var i = 0; i < entities.length; i++) {
-      //               if (entities[i] !== this) {
-      //                 $(entities[i]).css('background-color', '#ffb3b3');
-      //                 $(entities[i]).css('opacity', '0.75');
-      //               }
-      //             }
-      //             // highlight clicked entity yellow
-      //             $(this).css('background-color', '#ffff80');
-      //             $(this).css('opacity', '0.5');
-
-      //             // build analysis portion of entity
-      //             
-      //             wikiText = "";
-      //             var wikiImg = "";
-      //             // search matchedWords until entity found
-      //             var k = 0;
-      //             var entityFound = false;
-      //             while (k < matchedWords.length && !entityFound) {
-      //               var currentEntity = selectedEntities[k];
-      //               if (matchedWords[k].toLowerCase() == clickedEntity.toLowerCase()) {
-      //                 // log clicked entity
-      //                 var json;
-      //                 json = '{ "matchedText": "' + currentEntity["matchedText"] + '", ';
-      //                 json += '"type": "' + selectedEntityTypes[k] + '" } ';
-      //                 var clicked = JSON.parse(json);
-      //                 var logBlob = {
-      //                   time: Date.now() - 18000000,
-      //                   postID: postId.substring(5),
-      //                   clickedEntity: clicked,
-      //                 };
-      //                 var payLoad = {
-      //                   name: myName,
-      //                   logBlob: logBlob
-      //                 };
-      //                 chrome.runtime.sendMessage(payLoad, function(response){});
-
-      //                 } else { // if no wikiLink, add Google search button
-      //                   var link = "https://www.google.com/search?q=" +
-      //                     matchedWords[k].replace(/[&\\#,+'"()$=~%*{}]/g, ' ').split(' ').join('+');
-      //                   analysis += '</span><br><input type="button" value="Google Search" class="googlebutton" onclick="window.open(\'' + link + '\')" ';
-      //                   analysis += 'style="font-size:14px; font-weight:bold; background-color:#e5e5e5; border-radius:2px; border:1px solid #e4e4e4; color:#666; padding:10px; cursor:pointer;" onMouseOver="this.style.color=\'#333\'; this.style.background=\'#f5f5f5\'" onMouseOut="this.style.background=\'#e4e4e4\'; this.style.color=\'#666\'" />';
-      //                 }
-      //               }
-      //               k++;
-      //             }
-
-      //           });
-      //         }
-      //       }
-      //     }
-      //     if (!hasEntity && (linkSection || linkText.length == 0)) {
-      //       $("#white-box2").html("<p style='text-align:left'>No keywords detected.</p>");
-      //     }
-      //     var hidden = 1;
-      //     //only call this section once per click
-      //     if (linkSection) {
-      //       cleanText = translatedTextOrig + "<hr><p><b>Link:</b></p>" + translatedText;
-      //     } else {
-      //       cleanText = translatedText;
-      //     }
-      //     $("#white-box").empty();
-      //     $("#white-box").append(cleanText);
-      //     //slide entity box down when toggle is clicked only once
-      //     if (linkSection || linkText.length == 0) {
-      //       $("#entityToggle").unbind().click(function() {
-      //         // log button click
-      //         var logBlob = {
-      //           time: Date.now() - 18000000,
-      //           postID: postId.substring(5),
-      //           buttonClicked: "entity"
-      //         };
-      //         var payLoad = {
-      //           name: myName,
-      //           logBlob: logBlob
-      //         };
-      //         chrome.runtime.sendMessage(payLoad, function(response){});
-
-      //         $("#white-box2").slideToggle();
-      //         hidden++;
-      //         if (hidden % 2 == 0) { //change text when white-box2 is visible (even)
-      //           $("#white-box").empty();
-      //           if (newText) { //if newText exists
-      //             $("#white-box").append(newText); //change to translatedText
-      //             entityClick(); //get functionality for .entity clicks
-      //           } else {
-      //             $("#white-box").append(cleanText);
-      //           }
-      //         } else {
-      //           $("#white-box").empty(); //remove previous text
-      //           $("#white-box").append(cleanText); //change to highlightedText
-      //         }
-      //       });
-      //     }
-
-      //   },
-      //   error: function(err) {
-      //     $("#white-box").html("<p>Error. Please try again!</p>");
-      //   }
-      // });
     }
 
     var emotionAnalysisDone = false;
@@ -915,7 +609,6 @@ $(document).ready(function() {
 
         if(!emotionAnalysisDone){
             //run sentiment-analysis
-            //$("#translation-box").append("<h2>Sentiment Analysis </h2>");
             $("#translation-box").append("<div id = 'white-box3'></div>");
             $("#white-box3").css('background-color', 'white');
             $("#white-box3").css('height', 'auto');
@@ -939,11 +632,6 @@ $(document).ready(function() {
         } else {
             $("#white-box3").slideToggle();
         }
-
-        // $("#emotionToggle").click(function() {
-
-            
-        // });
     }
     
     function drawSentAnalysisBars(score) {
